c323b27b6f399a5e90ca0528a70199b7c715d6ab
soilihi ambdouroihmane <soilihiambdouroihmane@gmail.com>
2024-06-10 07:46:13 +0000

    update

---
+++ main.js
@@ -0,0 +1,27 @@
+// Boucle
+
+let i = 0;
+
+// Boucle while
+// while (i < 5){
+//     document.write("a ");
+//     i++;
+// }
+
+// Boucle repeter 
+// do {
+//     document.write("AAA ");
+//     i++;
+// }while(i<10);
+
+
+// Boucle pour
+for(let i=0; i<5, ++i){
+    document.write("a ");
+
+    if(i === 25)
+        break; // Sa interope le boucle et le mot cle continue qui ignore tout les instruction qui suit 
+}
+
+// version 2 de for
+//for...in et for...of
